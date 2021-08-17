8384dba49b886bda0c347d83a7cb85c46fe781cf
Crash <crashever@gmail.com>
2021-08-16 23:21:17 -0300

    resolvido problema com o cors

--- demo/waap-front-end/src/app/form-cadastro/form-cadastro.component.ts
+++ demo/waap-front-end/src/app/form-cadastro/form-cadastro.component.ts
@@ -1,54 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup,Validators } from '@angular/forms';
+import { FormBuilder, FormGroup } from "@angular/forms";
 import { FormCadastroService } from './form-cadastro.service';
+
+
+
 
 @Component({
   templateUrl: './form-cadastro.component.html',
   styleUrls: ['./form-cadastro.component.css']
 })
 export class FormCadastroComponent implements OnInit {
-  url = ''
-  cadastroForm!: FormGroup;
-  file: File;
-  preview: string;
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private formcadastroService: FormCadastroService) { }
 
-  ngOnInit() {
-    this.cadastroForm = this.formBuilder.group({
-      file: ['', Validators.required],
-      nome: ['', Validators.maxLength(300)],
-      email: ['', Validators.maxLength(300)],
-      telefone: ['', Validators.maxLength(300)]
 
-    })
+  form: FormGroup;
+  API = "http://localhost:8080/users/upload";
+  selectedFile: File;
+
+  constructor(private formcadastroService: FormCadastroService) { }
+
+  ngOnInit() { }
+
+  onFileChanged(event:any) {
+    this.selectedFile = event.target.files[0]
   }
 
-  teste(event:Event) {
+  onUpload() {
+    this.formcadastroService.onUpload(this.selectedFile);
+  }
+
+
+
 }
 
-  submit() {
-    const nome:string = this.cadastroForm.get('nome').value;
-    const email:string = this.cadastroForm.get('email').value;
-    const telefone:number = this.cadastroForm.get('email').value;
-  }
-
-
-  handleFile(event: Event) {
-    const target = event.target as HTMLInputElement;
-    const files = target.files as FileList;
-    console.log(files);
-    const reader = new FileReader();
-    reader.onload = (event: any) => this.preview = event.target.result;
-    // reader.readAsDataURL(files);
-  }
-
-  upload() {
-    this.formcadastroService
-      .upload(this.file)
-      .subscribe((a)=>{console.log('finished',a);})
-  }
-
-}
--- demo/waap-front-end/src/app/form-cadastro/form-cadastro.service.ts
+++ demo/waap-front-end/src/app/form-cadastro/form-cadastro.service.ts
@@ -1,17 +1,31 @@
 import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
+import { FormGroup } from '@angular/forms';
 
-const API = 'http://localhost:8081';
+const API = "http://localhost:8080/users/upload";
 @Injectable({
   providedIn: 'root',
 })
 export class FormCadastroService {
   constructor(private http: HttpClient) {}
 
-  upload(file: File) {
-    const formData = new FormData();
-    formData.append('foto', file);
 
-    return this.http.post(API + '/fotos', formData);
+  onUpload(selectedFile:File) {
+    const uploadData = new FormData();
+    uploadData.append('image', selectedFile, selectedFile.name);
+    this.http.post(API, uploadData, {
+      reportProgress: true,
+      observe: 'events'
+    })
+      .subscribe(event => {
+        console.log(event); // handle event here
+      });
   }
+
+
+  getPhoto(id:string) {
+    return this.http.get(API + '/users/image/' + id);
+    
+  }
+
 }
